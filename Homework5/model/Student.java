358ea7373e8cb9d231e059023c95028052b31b64
ZHenya <saltykovazh@mail.ru>
2023-06-22 21:05:04 +0400

    Homework5

---
+++ Homework5/model/Student.java
@@ -0,0 +1,30 @@
+package Homework5.model;
+
+import lombok.*;
+
+@Getter
+//@AllArgsConstructor
+//@Setter
+//@ToString
+@EqualsAndHashCode(callSuper = true)
+//@Data
+//@ToString
+public class Student extends User implements Comparable<Student> {
+
+    private Double gpa;
+    private String advisor;
+
+    public Student(Long id, String fullName, Integer age, String phoneNumber) {
+        super(id, fullName, age, phoneNumber);
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        return getFullName().compareTo(o.getFullName());
+    }
+
+    @Override
+    public String toString() {
+        return String.format("id: %s, ФИО: %s, Возраст: %s", this.getId(), this.getFullName(), this.getAge());
+    }
+}
