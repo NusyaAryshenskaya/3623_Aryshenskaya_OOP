1ef25c65e881d63a57a878acffd18c220b572f60
ZHenya <saltykovazh@mail.ru>
2023-06-22 21:29:06 +0400

    Homework5

---
+++ Homework5/view/SortType.java
@@ -0,0 +1,9 @@
+package Homework5.view;
+
+public interface SortType {
+    String NONE = "none";
+    String FAMILY = "family_name";
+    String NAME = "user_name";
+    String AGE = "age";
+    String EXPERIENCE = "experience";
+}
